95a2eb3576afff8878af5aa203da0f810df9af8b
lkarine <141235835+lkarine@users.noreply.github.com>
2025-05-26 09:27:10 -0300

    f

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPerguntas = document.querySelector("caixa-perguuntas");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
 const caixaAlternativas = document.querySelector("caixa-alternativas");
 const caixaResultado = document.querySelector("caixa-resultado");
 const textoResultado = document.querySelector("texto-resultado");
